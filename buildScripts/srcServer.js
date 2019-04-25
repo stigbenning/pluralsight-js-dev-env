0ce05685f30ef4ee3586c4cb192c247c9203fe95
Stig Benning <stig.benning@yahoo.com>
2019-04-25 13:03:04 +0100

    html port and command text
    Updated the html port to 3000 and added descriptions to command.txt

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var open = require('open');
 
-var port = 3800;
+var port = 3000;
 var app = express();
 
 app.get('/', function(req, res){
